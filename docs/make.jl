cf1318680f511223078a01fa0f861df6155475ba
Justin Willmert <justin@willmert.me>
2021-01-01 14:20:20 -0600

    Workaround autodocs including the entire low-level API

--- docs/make.jl
+++ docs/make.jl
@@ -2,6 +2,15 @@ using Documenter
 using HDF5
 using MPI  # needed to generate docs for parallel HDF5 API
 
+# Used in index.md to filter the autodocs list
+wherefrom(m::Method) = String(m.file)
+not_from_api(m::Method) = !endswith(wherefrom(m), "src/api.jl")
+not_from_api(f::Function) = all(not_from_api, methods(f))
+not_from_api(o) = true
+# Manually-defined low-level API
+not_from_api(::typeof(HDF5.h5p_get_class_name)) = false
+not_from_api(::typeof(HDF5.h5t_get_member_name)) = false
+not_from_api(::typeof(HDF5.h5t_get_tag)) = false
 
 makedocs(;
     modules=[HDF5],
